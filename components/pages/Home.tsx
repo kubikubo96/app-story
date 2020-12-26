ee64909487995479d0d63b2ce3d63f7856321d1a
kubikubo <tien.nguyentat.1@gmail.com>
2020-12-26 20:04:36 +0700

    "add Navigation"

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
 
 const Home = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me  🎉
-      </Text>
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 };
 
